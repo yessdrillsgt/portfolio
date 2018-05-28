44ad4addfc7d25141519b9580ef052c7e8a35548
Justin <yessdrillsgt@yahoo.com>
2018-05-28 19:33:45 -0400

    Updated about section

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, function(){
+    console.log("\nServer listening on port " + port);
+});
